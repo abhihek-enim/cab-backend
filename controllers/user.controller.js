89dc2d48e5904a0b860ac54969f562331872b7f5
abhishek <shukla13122000@gmail.com>
2024-11-27 20:11:27 +0530

    add user profile end point, add auth middleware

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -39,3 +39,7 @@ module.exports.loginUser = async (req, res, next) => {
   const token = user.generateAuthToken();
   return res.status(200).json({ token, user });
 };
+
+module.exports.getUserProfile = async (req, res, next) => {
+  return res.status(200).json(req.user);
+};
